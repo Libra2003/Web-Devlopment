b779a5a5ea60f7c1520bf6d5f1aa3d6749938bcd
zaint2755@gmail.com <75733607+Libra2003@users.noreply.github.com>
2022-11-20 23:10:37 +0500

    More commits

---
+++ NodeJS/Serving-html-Files.js
@@ -0,0 +1,13 @@
+//How to serve files accroding to the user requirement
+const http = require('http')
+const fs = require('fs')
+const fileContent = fs.readFileSync('Math-Objects.html')
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text/html'});
+    res.end(fileContent)
+})
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("Listening on port 8000");
+})
